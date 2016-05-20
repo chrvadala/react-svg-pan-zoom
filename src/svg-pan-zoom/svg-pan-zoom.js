b33f7a3459473e7341aa63939156d38ee775e9de
Christian Vadalà <chr.vadala@gmail.com>
2016-05-20 18:28:57 +0200

    adds artboard/paper background

--- src/svg-pan-zoom/svg-pan-zoom.js
+++ src/svg-pan-zoom/svg-pan-zoom.js
@@ -5,10 +5,6 @@ class SvgPanZoom extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-
-  }
-
   render() {
     return (
       <svg
@@ -16,7 +12,25 @@ class SvgPanZoom extends React.Component {
         height={this.props.artboardHeight}
         style={this.props.style}
       >
-        {this.props.children}
+
+        <rect
+          fill={this.props.artboardBackground}
+          x={0}
+          y={0}
+          width={this.props.artboardWidth}
+          height={this.props.artboardHeight}/>
+
+        <g ref="paper">
+          <rect
+            fill={this.props.paperBackground}
+            x={0}
+            y={0}
+            width={this.props.paperWidth}
+            height={this.props.paperHeight}/>
+          <g ref="content">
+            {this.props.children}
+          </g>
+        </g>
       </svg>
     );
   }
@@ -29,18 +43,26 @@ SvgPanZoom.propTypes = {
   //height of the container displayed on screen
   artboardHeight: React.PropTypes.number.isRequired,
 
+  //background of the artboard
+  artboardBackground: React.PropTypes.string,
+
   //width of the paper
   paperWidth: React.PropTypes.number.isRequired,
 
   //height of the paper
   paperHeight: React.PropTypes.number.isRequired,
 
+  //background of the paper
+  paperBackground: React.PropTypes.string,
+
   //style of the SVG tag
   style: React.PropTypes.object
 };
 
 SvgPanZoom.defaultProps = {
-  style: {}
+  style: {},
+  artboardBackground: "#616264",
+  paperBackground: "#fff"
 };
 
 export default SvgPanZoom;
