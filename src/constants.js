afe79c388aff23e590a46abe3b0aa0819c6952a7
Christian Vadalà <chr.vadala@gmail.com>
2016-05-27 11:16:52 +0200

    changed constants value

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,6 @@ export const MODE_IDLE = 'idle';
 export const MODE_PANNING = 'panning';
 export const MODE_ZOOMING = 'zooming';
 
-export const TOOL_NONE = 'tool_none';
-export const TOOL_ZOOM = 'tool_zoom';
-export const TOOL_PAN = 'tool_pan';
+export const TOOL_NONE = 'none';
+export const TOOL_ZOOM = 'zoom';
+export const TOOL_PAN = 'pan';
