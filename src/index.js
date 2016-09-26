d830e2b11d3a94ce1c59cf73849d8784e084cd56
Christian Vadalà <chr.vadala@gmail.com>
2016-09-26 19:11:10 +0200

    adds credits on console log

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ViewerHelper from './viewer-helper';
 import Toolbar from './toolbar';
 import {TOOL_NONE, TOOL_PAN, TOOL_ZOOM, TOOL_ZOOM_IN, TOOL_ZOOM_OUT}from './constants';
 
+console.info("react-svg-pan-zoom started"); //MIT LICENSE COMPLIANT
+
 export {
   Viewer,
   ViewerHelper,
