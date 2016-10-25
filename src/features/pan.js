3598019796ce83b2e4bf0e21a09480830d31b444
Christian Vadalà <chr.vadala@gmail.com>
2016-10-25 15:16:30 +0200

    improve pan feature

--- src/features/pan.js
+++ src/features/pan.js
@@ -1,22 +1,33 @@
 import {MODE_IDLE, MODE_PANNING} from '../constants';
-import {getDefaultValue, set} from './common';
+import {getDefaultValue, set, getSVGPoint} from './common';
 import {Matrix} from 'transformation-matrix-js';
 
 /**
  *
  * @param value
- * @param viewerDeltaX
- * @param viewerDeltaY
+ * @param SVGDeltaX
+ * @param SVGDeltaY
+ * @param panLimit
  * @returns {Object}
  */
-export function pan(value, viewerDeltaX, viewerDeltaY) {
+export function pan(value, SVGDeltaX, SVGDeltaY, panLimit = undefined) {
   let {a, b, c, d, e, f} = value;
   let matrix = Matrix.from(a, b, c, d, e, f);
 
   let act = new Matrix();
-  act = act.translate(viewerDeltaX, viewerDeltaY);
+  act = act.translate(SVGDeltaX, SVGDeltaY);
 
   matrix = matrix.multiply(act);
+
+  // apply pan limits
+  if(panLimit) {
+    let zoomLevel = matrix.decompose(false).scale.x;
+    matrix.e = Math.min(matrix.e, value.viewerWidth - panLimit);
+    matrix.e = Math.max(matrix.e, panLimit - value.SVGWidth * zoomLevel);
+
+    matrix.f = Math.min(matrix.f, value.viewerHeight - panLimit);
+    matrix.f = Math.max(matrix.f, panLimit - value.SVGHeight * zoomLevel);
+  }
 
   return set(value, {
     mode: MODE_IDLE,
@@ -40,36 +51,19 @@ export function startPanning(value, viewerX, viewerY) {
 export function updatePanning(value, viewerX, viewerY, panLimit) {
   if (value.mode !== MODE_PANNING) throw new Error('update pan not allowed in this mode ' + value.mode);
 
-  let {a, b, c, d, e, f, startX, startY} = value;
-  let matrix = Matrix.from(a, b, c, d, e, f);
-  let zoomLevel = matrix.decompose(false).scale.x;
+  let {startX, startY} = value;
 
-  let deltaX = (startX - viewerX) / zoomLevel;
-  let deltaY = (startY - viewerY) / zoomLevel;
+  let start = getSVGPoint(value, startX, startY);
+  let end = getSVGPoint(value, viewerX, viewerY);
 
-  let act = new Matrix();
-  act = act.translate(-deltaX, -deltaY);
+  let deltaX = end.x - start.x;
+  let deltaY = end.y - start.y;
 
-  matrix = matrix.multiply(act);
-
-  //apply pan limits
-  matrix.e = Math.min(matrix.e, value.viewerWidth - panLimit);
-  matrix.e = Math.max(matrix.e, panLimit - value.SVGWidth * zoomLevel);
-
-  matrix.f = Math.min(matrix.f, value.viewerHeight - panLimit);
-  matrix.f = Math.max(matrix.f, panLimit - value.SVGHeight * zoomLevel);
-
-  return set(value, {
+  let nextValue = pan(value, deltaX, deltaY, panLimit);
+  return set(nextValue, {
     mode: MODE_PANNING,
     startX: viewerX,
     startY: viewerY,
-    a: matrix.a,
-    b: matrix.b,
-    c: matrix.c,
-    d: matrix.d,
-    e: matrix.e,
-    f: matrix.f
-
   });
 }
 
