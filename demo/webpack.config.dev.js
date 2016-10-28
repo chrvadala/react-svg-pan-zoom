5867b3405933ddef67937fd6610daa55e52bc046
Christian Vadalà <chr.vadala@gmail.com>
2016-10-28 15:59:03 +0200

    optimize webpack

--- demo/webpack.config.dev.js
+++ demo/webpack.config.dev.js
@@ -22,7 +22,7 @@ module.exports = {
     loaders: [
       {
         test: /\.(jsx|js)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel'
       }
     ]
