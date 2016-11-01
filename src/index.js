cc1f51a691d5e074f4e9a066bb2f45f995ef342f
Christian Vadalà <chr.vadala@gmail.com>
2016-11-01 15:03:07 +0100

    adds zoomOnViewerCenter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactSVGPanZoom from './viewer';
 import Toolbar from './ui-toolbar/toolbar';
 import {pan} from './features/pan';
-import {zoom, fitSelection, fitToViewer} from './features/zoom';
+import {zoom, fitSelection, fitToViewer, zoomOnViewerCenter} from './features/zoom';
 import {
   TOOL_NONE, TOOL_PAN, TOOL_ZOOM_IN, TOOL_ZOOM_OUT,
   POSITION_NONE, POSITION_TOP, POSITION_RIGHT, POSITION_BOTTOM, POSITION_LEFT
@@ -11,7 +11,7 @@ console.info("react-svg-pan-zoom started"); //MIT LICENSE COMPLIANT
 
 export {
   ReactSVGPanZoom, Toolbar,
-  pan, zoom, fitSelection, fitToViewer,
+  pan, zoom, fitSelection, fitToViewer, zoomOnViewerCenter,
   TOOL_NONE, TOOL_PAN, TOOL_ZOOM_IN, TOOL_ZOOM_OUT,
   POSITION_NONE, POSITION_TOP, POSITION_RIGHT, POSITION_BOTTOM, POSITION_LEFT
 };
