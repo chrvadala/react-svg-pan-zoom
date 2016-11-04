bf6577e39fa039ec4673e9f372f8215a2f48fb5c
Christian Vadalà <chr.vadala@gmail.com>
2016-11-04 13:22:51 +0100

    adds deprecated message

--- src/index.js
+++ src/index.js
@@ -9,7 +9,17 @@ import {
 
 console.info("react-svg-pan-zoom started"); //MIT LICENSE COMPLIANT
 
+const Viewer = () => {
+  let msg = "HEY! You are trying to use and older version of ReactSVGPanZoom. "
+   + "Read here https://github.com/chrvadala/react-svg-pan-zoom/wiki/Migrate-v1-to-v2";
+
+  console.error(msg);
+  return null;
+};
+
+
 export {
+  Viewer, //deprecated
   ReactSVGPanZoom, Toolbar,
   pan, zoom, fitSelection, fitToViewer, zoomOnViewerCenter,
   TOOL_NONE, TOOL_PAN, TOOL_ZOOM_IN, TOOL_ZOOM_OUT,
