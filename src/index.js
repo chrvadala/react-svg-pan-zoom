d8e04a0ec19a48a55f1a55f48d7bd24ace5198e8
Christian Vadalà <chr.vadala@gmail.com>
2017-03-12 23:00:05 +0100

    improve index

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-import ReactSVGPanZoom from './viewer';
-import Toolbar from './ui-toolbar/toolbar';
-import {setPointOnViewerCenter, reset} from './features/common';
-import {pan} from './features/pan';
-import {zoom, fitSelection, fitToViewer, zoomOnViewerCenter} from './features/zoom';
-import {
-  TOOL_AUTO, TOOL_NONE, TOOL_PAN, TOOL_ZOOM_IN, TOOL_ZOOM_OUT,
-  POSITION_NONE, POSITION_TOP, POSITION_RIGHT, POSITION_BOTTOM, POSITION_LEFT
-} from './constants';
+export {default as ReactSVGPanZoom} from './viewer';
+export {default as Toolbar} from './ui-toolbar/toolbar';
+export {setPointOnViewerCenter, reset} from './features/common';
+export {pan} from './features/pan';
+export {zoom, fitSelection, fitToViewer, zoomOnViewerCenter} from './features/zoom';
+export * from './constants';
 
-const Viewer = () => {
+export const Viewer = () => {
   let msg = "HEY! You are trying to use an older version of ReactSVGPanZoom. "
-   + "Read here https://github.com/chrvadala/react-svg-pan-zoom/blob/master/docs/migrate-from-v1-to-v2.md";
+    + "Read here https://github.com/chrvadala/react-svg-pan-zoom/blob/master/docs/migrate-from-v1-to-v2.md";
 
   console.error(msg);
   return null;
-};
-
-
-export {
-  Viewer, //deprecated
-  ReactSVGPanZoom, Toolbar,
-  pan, zoom, fitSelection, fitToViewer, zoomOnViewerCenter, setPointOnViewerCenter, reset,
-  TOOL_AUTO, TOOL_NONE, TOOL_PAN, TOOL_ZOOM_IN, TOOL_ZOOM_OUT,
-  POSITION_NONE, POSITION_TOP, POSITION_RIGHT, POSITION_BOTTOM, POSITION_LEFT
 };
