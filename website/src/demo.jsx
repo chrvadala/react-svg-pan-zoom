aa230613a26266083e32372baff49ea921fd55c7
Christian Vadalà <chr.vadala@gmail.com>
2017-04-24 18:10:34 +0200

    adds miniature config on demo

--- website/src/demo.jsx
+++ website/src/demo.jsx
@@ -28,6 +28,8 @@ export default class Demo extends React.Component {
       detectWheel: true,
       toolbarPosition: POSITION_RIGHT,
       preventPanOutside: true,
+      miniatureWidth: "",
+      miniaturePosition: POSITION_LEFT,
     };
 
     this.Viewer = null;
@@ -91,6 +93,9 @@ export default class Demo extends React.Component {
               onMouseDown={event => console.info('down', event.x, event.y)}
               onDoubleClick={event => console.info('dblclick', event.x, event.y)}
 
+              miniaturePosition={this.state.miniaturePosition}
+              miniatureWidth={isNaN(parseFloat(this.state.miniatureWidth)) ? undefined : parseFloat(this.state.miniatureWidth)}
+
               onTouchStart={event => {
                 event.preventDefault();
                 console.info('touchstart', 'points' + event.points.map(({x, y, identifier}) => `${x} ${y} ${identifier}`))
@@ -121,22 +126,26 @@ export default class Demo extends React.Component {
               <h6>Additional features</h6>
               <div className="form-check">
                 <label className="form-check-label">
-                  <input className="form-check-input" type="checkbox" checked={this.state.detectWheel} name="detectWheel"
+                  <input className="form-check-input" type="checkbox" checked={this.state.detectWheel}
+                         name="detectWheel"
                          onChange={ event => this.setState({detectWheel: event.target.checked})}/> detectWheel
                 </label>
               </div>
 
               <div className="form-check">
                 <label className="form-check-label">
-                  <input className="form-check-input" type="checkbox" checked={this.state.detectAutoPan} name="detectAutoPan"
+                  <input className="form-check-input" type="checkbox" checked={this.state.detectAutoPan}
+                         name="detectAutoPan"
                          onChange={ event => this.setState({detectAutoPan: event.target.checked})}/> detectAutoPan
                 </label>
               </div>
 
               <div className="form-check">
                 <label className="form-check-label">
-                  <input className="form-check-input" type="checkbox" checked={this.state.preventPanOutside} name="preventPanOutside"
-                         onChange={ event => this.setState({preventPanOutside: event.target.checked})}/> preventPanOutside
+                  <input className="form-check-input" type="checkbox" checked={this.state.preventPanOutside}
+                         name="preventPanOutside"
+                         onChange={ event => this.setState({preventPanOutside: event.target.checked})}/>
+                  preventPanOutside
                 </label>
               </div>
             </div>
@@ -169,6 +178,34 @@ export default class Demo extends React.Component {
                     <option value={TOOL_ZOOM_IN}>zoom in</option>
                     <option value={TOOL_ZOOM_OUT}>zoom out</option>
                   </select>
+                </div>
+              </div>
+            </div>
+
+            <hr/>
+
+            <h6>Miniature</h6>
+
+            <div className="row">
+              <div className="col-sm-6">
+                <h6>Position</h6>
+                <div className="form-group">
+                  <select value={this.state.miniaturePosition} className="form-control" name="miniaturePosition"
+                          onChange={ event => this.setState({miniaturePosition: event.target.value})}>
+                    <option value={POSITION_NONE}>none</option>
+                    <option value={POSITION_RIGHT}>right</option>
+                    <option value={POSITION_LEFT}>left</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="col-sm-6">
+                <h6>Width <span
+                  className="text-muted"> ({isNaN(parseFloat(this.state.miniatureWidth)) ? 'default' : parseFloat(this.state.miniatureWidth)})</span>
+                </h6>
+                <div className="form-group">
+                  <input type="text" value={this.state.miniatureWidth} className="form-control" name="miniatureWidth"
+                         onChange={ event => this.setState({miniatureWidth: event.target.value})}/>
                 </div>
               </div>
             </div>
@@ -234,23 +271,28 @@ export default class Demo extends React.Component {
               <div style={{marginBottom: "3px"}}>
                 <div>Select tool:</div>
 
-                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON} name="select-tool-auto-btn"
+                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON}
+                        name="select-tool-auto-btn"
                         onClick={event => this.Viewer.changeTool(TOOL_AUTO)}> auto
                 </button>
 
-                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON} name="select-tool-none-btn"
+                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON}
+                        name="select-tool-none-btn"
                         onClick={event => this.Viewer.changeTool(TOOL_NONE)}> none
                 </button>
 
-                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON} name="select-tool-pan-btn"
+                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON}
+                        name="select-tool-pan-btn"
                         onClick={event => this.Viewer.changeTool(TOOL_PAN)}> pan
                 </button>
 
-                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON} name="select-tool-zoom-in-btn"
+                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON}
+                        name="select-tool-zoom-in-btn"
                         onClick={event => this.Viewer.changeTool(TOOL_ZOOM_IN)}> zoom in
                 </button>
 
-                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON} name="select-tool-zoom-out-btn"
+                <button type="button" className="btn btn-outline-primary btn-sm" style={STYLE_BUTTON}
+                        name="select-tool-zoom-out-btn"
                         onClick={event => this.Viewer.changeTool(TOOL_ZOOM_OUT)}> zoom out
                 </button>
               </div>
