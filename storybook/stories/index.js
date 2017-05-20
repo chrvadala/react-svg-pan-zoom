21aa90604b54226c7cef4857d696744f823fa12f
Christian Vadalà <chr.vadala@gmail.com>
2017-05-20 12:10:20 +0200

    adds MainStory

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -4,14 +4,16 @@ import {storiesOf} from '@storybook/react';
 import {withKnobs} from '@storybook/addon-knobs';
 
 import Welcome from './Welcome';
+import MainStory from './MainStory';
 import AutoModeStory from './AutoModeStory'
 import MethodsStory from './MethodsStory'
 import AutosizerViewer from './AutosizerViewer'
 import WithToolbar from './WithToolbar'
 
-storiesOf('<ReactSVGPanZoom' + '>', module)
+storiesOf('React SVG Pan Zoom', module)
   .addDecorator(withKnobs)
   .add('README', () => <Welcome/>)
+  .add('Viewer', () => <MainStory/>)
   .add('Viewer with tool auto', () => <AutoModeStory/>)
   .add('Call methods', () => <MethodsStory/>)
   .add('Viewer with toolbar', () => <WithToolbar/>)
