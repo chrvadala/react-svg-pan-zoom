a939be51ddafdfb5592c1d6ba60163df6c4e7e87
Christian Vadalà <chr.vadala@gmail.com>
2017-05-20 12:26:05 +0200

    adds options

--- storybook/addons.js
+++ storybook/addons.js
@@ -1,16 +1,9 @@
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
+import '@storybook/addon-options/register';
 import addonAPI from '@storybook/addons';
 
-let width = window.innerWidth;
-
 addonAPI.register('chrvadala/options', (storybookAPI) => {
-  storybookAPI.setOptions({
-    name: 'React SVG Pan Zoom',
-    url: 'https://github.com/chrvadala/react-svg-pan-zoom',
-    downPanelInRight: width > 1100,
-  });
-
   let fullscreen = false;
   window.toggleFullscreen = function () {
     fullscreen = !fullscreen;
