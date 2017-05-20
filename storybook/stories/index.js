42a4970a23f5e14833782f64182838bfefcd6b8c
Christian Vadalà <chr.vadala@gmail.com>
2017-05-20 12:14:07 +0200

    remove AutoModeStory and WithToolbarStory

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -5,18 +5,14 @@ import {withKnobs} from '@storybook/addon-knobs';
 
 import Welcome from './Welcome';
 import MainStory from './MainStory';
-import AutoModeStory from './AutoModeStory'
 import MethodsStory from './MethodsStory'
 import AutosizerViewer from './AutosizerViewer'
-import WithToolbar from './WithToolbar'
 
 storiesOf('React SVG Pan Zoom', module)
   .addDecorator(withKnobs)
   .add('README', () => <Welcome/>)
   .add('Viewer', () => <MainStory/>)
-  .add('Viewer with tool auto', () => <AutoModeStory/>)
   .add('Call methods', () => <MethodsStory/>)
-  .add('Viewer with toolbar', () => <WithToolbar/>)
   .add('Autosizer viewer', () => <AutosizerViewer />)
 
 
