2e3b0a07eceea7b31f652c8639371b1f2008fda9
Dmitriy <DmitriySpirit@users.noreply.github.com>
2017-09-14 13:08:38 +0300

    Export Miniature in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export {default as ReactSVGPanZoom} from './viewer';
 export {default as Toolbar} from './ui-toolbar/toolbar';
+export {default as Miniature} from './ui-miniature/miniature';
 export {setPointOnViewerCenter, reset} from './features/common';
 export {pan} from './features/pan';
 export {zoom, fitSelection, fitToViewer, zoomOnViewerCenter} from './features/zoom';
