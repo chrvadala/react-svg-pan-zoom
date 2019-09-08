d1ac7ec4a4957a736d881558348d44a357d5f9ed
TimVanMourik <timvanmourik@gmail.com>
2019-09-08 16:26:55 +0200

    removing reduncdant functions
    All these functions pass on their values to the ref

--- src/uncontrolled-viewer.js
+++ src/uncontrolled-viewer.js
@@ -7,42 +7,6 @@ const UncontrolledReactSVGPanZoom = forwardRef((props, Viewer) => {
   const [value, setValue] = useState(props.defaultValue || {});
   const [tool, setTool] = useState(props.defaultTool || TOOL_NONE);
 
-  function pan(SVGDeltaX, SVGDeltaY) {
-    Viewer.pan(SVGDeltaX, SVGDeltaY)
-  }
-
-  function zoom(SVGPointX, SVGPointY, scaleFactor) {
-    Viewer.zoom(SVGPointX, SVGPointY, scaleFactor)
-  }
-
-  function fitSelection(selectionSVGPointX, selectionSVGPointY, selectionWidth, selectionHeight) {
-    Viewer.fitSelection(selectionSVGPointX, selectionSVGPointY, selectionWidth, selectionHeight)
-  }
-
-  function fitToViewer(SVGAlignX, SVGAlignY) {
-    Viewer.fitToViewer(SVGAlignX, SVGAlignY)
-  }
-
-  function zoomOnViewerCenter(scaleFactor) {
-    Viewer.zoomOnViewerCenter(scaleFactor)
-  }
-
-  function setPointOnViewerCenter(SVGPointX, SVGPointY, zoomLevel) {
-    Viewer.setPointOnViewerCenter(SVGPointX, SVGPointY, zoomLevel)
-  }
-
-  function reset() {
-    Viewer.reset()
-  }
-
-  function openMiniature() {
-    Viewer.openMiniature()
-  }
-
-  function closeMiniature() {
-    Viewer.closeMiniature()
-  }
-
   const {width, height, onChangeTool, onChangeValue, ...svgProps} = props;
 
   return (
