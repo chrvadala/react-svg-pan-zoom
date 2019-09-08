24ccf2e29afe460afd9380455cf4c6e1ad7e8647
TimVanMourik <timvanmourik@gmail.com>
2019-09-08 11:57:04 +0200

    hookifying ToolbarButton

--- src/ui-toolbar/toolbar-button.jsx
+++ src/ui-toolbar/toolbar-button.jsx
@@ -1,70 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {
   POSITION_TOP, POSITION_BOTTOM
 } from '../constants';
 
-export default class ToolbarButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {hover: false};
-  }
+const ToolbarButton = (props) => {
+  const [hover, setHover] = useState(false)
 
-  change(event) {
+  function change(event) {
     event.preventDefault();
     event.stopPropagation();
 
     switch (event.type) {
       case 'mouseenter':
       case 'touchstart':
-        this.setState({hover: true});
+        setHover(true);
         break;
       case 'mouseleave':
       case 'touchend':
       case 'touchcancel':
-        this.setState({hover: false});
+        setHover(false);
         break;
     }
   }
 
-  render() {
-    let style = {
-      display: "block",
-      width: "24px",
-      height: "24px",
-      margin: [POSITION_TOP, POSITION_BOTTOM].indexOf(this.props.toolbarPosition) >= 0 ? "2px 1px" : "1px 2px",
-      color: this.props.active || this.state.hover ? '#1CA6FC' : '#FFF',
-      transition: "color 200ms ease",
-      background: "none",
-      padding: "0px",
-      border: "0px",
-      outline: "0px",
-      cursor: "pointer"
-    };
+  const {title, name, active, toolbarPosition, onClick, children} = props;
 
-    return (
-      <button
-        onMouseEnter={e => this.change(e)}
-        onMouseLeave={e => this.change(e)}
+  let style = {
+    display: "block",
+    width: "24px",
+    height: "24px",
+    margin: [POSITION_TOP, POSITION_BOTTOM].indexOf(toolbarPosition) >= 0 ? "2px 1px" : "1px 2px",
+    color: active || hover ? '#1CA6FC' : '#FFF',
+    transition: "color 200ms ease",
+    background: "none",
+    padding: "0px",
+    border: "0px",
+    outline: "0px",
+    cursor: "pointer"
+  };
 
-        onTouchStart={e => {
-          this.change(e);
-          this.props.onClick(e);
-        }}
-        onTouchEnd={e => this.change(e)}
-        onTouchCancel={e => this.change(e)}
+  return (
+    <button
+      onMouseEnter={e => change(e)}
+      onMouseLeave={e => change(e)}
 
-        onClick={this.props.onClick}
+      onTouchStart={e => {
+        change(e);
+        onClick(e);
+      }}
+      onTouchEnd={e => change(e)}
+      onTouchCancel={e => change(e)}
 
-        style={style}
-        title={this.props.title}
-        name={this.props.name}
-        role="button"
-        type="button"
-      >{this.props.children}</button>
-    )
-  }
+      onClick={onClick}
 
+      style={style}
+      title={title}
+      name={name}
+      role="button"
+      type="button"
+    >{children}</button>
+  )
 }
 
 ToolbarButton.propTypes = {
@@ -74,3 +70,5 @@ ToolbarButton.propTypes = {
   onClick: PropTypes.func.isRequired,
   active: PropTypes.bool.isRequired
 };
+
+export default ToolbarButton;
