9b9d4675c38efc0a90d14c15df6567b8ebbb5a3c
Christian <chr.vadala@gmail.com>
2019-12-01 16:21:26 +0100

    code clean

--- src/events/viewer-mouse-event.js
+++ src/events/viewer-mouse-event.js
@@ -4,7 +4,7 @@ import {getMousePosition} from "../features/interactions";
 export default class ViewerMouseEvent extends ViewerEvent {
   get point() {
     if (!this._cachePoint) {
-      const {originalEvent: event, value, SVGViewer} = this;
+      const {originalEvent: event, SVGViewer} = this;
       this._cachePoint = getMousePosition(event, SVGViewer)
     }
     return this._cachePoint;
